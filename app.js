958e00941f71d595591b89f041bfb713a6530419
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2021-03-14 11:48:09 -0300

    :sparkles:  Adicionando menu mobile

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const menuIcon = document.querySelector('.nav__menu-mobile i')
+const body = document.body
+const menu = document.querySelector('.menu')
+const menuIcon = document.querySelector('.menu__mobile i')
 
 menuIcon.addEventListener('click', () => {
   const menuIconClass = menuIcon.getAttribute('class')
+
+  menu.classList.toggle('menu--active')
+
+  body.classList.toggle('body--overflow')
 
   if (menuIconClass === 'fas fa-bars') {
     menuIcon.setAttribute('class', 'fas fa-times')
